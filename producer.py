9e12eaf9e24dd32960676f36f13a0520ebb58a75
fomars <fomars@yandex-team.ru>
2020-11-19 01:53:25 +0300

    async producer, added resp timeout

--- producer.py
+++ producer.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import re
+import signal
 import time
 from queue import Queue, Empty
 from threading import Thread, Event
@@ -24,55 +25,101 @@ def kafka_producer(queue, period):
     )
     while True:
         try:
-            message = queue.get(timeout=period)
+            data_dict = queue.get(timeout=period)
         except Empty:
             continue
-        if message is not None:
-            print('Sending {}'.format(message))
-            producer.send('website_availability', message.encode('utf-8'))
+        if data_dict is not None:
+            print('Sending {}'.format(data_dict))
+            producer.send('website_availability', json.dumps(data_dict).encode('utf-8'))
             producer.flush()
         else:
             break
 
 
-def check_website(url, regex):
-    """
-    :type regex: str
-    :type url: str
-    """
-    if not url.startswith('http'):
-        url = f'http://{url}'
-    resp = requests.get(url)
-    return {'ts': time.time(),
-            'response_time_ms': resp.elapsed.total_seconds() * 1000,
-            'http_code': resp.status_code,
-            'regex_matches': re.findall(regex.encode('utf-8'), resp.content)}
+def get_scheduler(period):
+    t = time.time()
+    while True:
+        yield t
+        t += period
+
+
+class Poller(Thread):
+    def __init__(self, url, timeout, scheduler, stop_event, queue, regex=None):
+        """
+        :type queue: Queue
+        :type regex: str
+        :type url: str
+        :type scheduler: generator
+        :type stop_event: Event
+        """
+        super().__init__()
+        self.url = url if url.startswith('http') else f'http://{url}'
+        self.timeout = timeout
+        self.scheduler = scheduler
+        self.stop_event = stop_event
+        self.results_queue = queue
+        self.regex_b = regex.encode('utf-8') if regex else None
+
+    def run(self):
+        while not self.stop_event.is_set():
+            start_time = next(self.scheduler)
+            while time.time() < start_time:
+                if not self.stop_event.is_set():
+                    pass
+                else:
+                    return
+            self.results_queue.put(self.check_website())
+
+    def check_website(self):
+        ts = time.time()
+        try:
+            resp = requests.get(self.url, timeout=self.timeout)
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            return {'ts': ts,
+                    'response_time_ms': time.time() - ts,
+                    'http_code': 0,
+                    'regex_matches': []}
+        return {'ts': ts,
+                'response_time_ms': resp.elapsed.total_seconds() * 1000,
+                'http_code': resp.status_code,
+                'regex_matches': re.findall(self.regex_b, resp.content) if self.regex_b else []}
+
+
+def set_interrupt_handler(stop_event, pollers, queue, results_sender):
+    def interrupt_handler(signum, frame):
+        print('Signal handler called with signal', signum)
+        stop_event.set()
+        for poller in pollers:
+            poller.join()
+        queue.put(None)
+        results_sender.join()
+
+    signal.signal(signal.SIGINT, interrupt_handler),
+    signal.signal(signal.SIGTERM, interrupt_handler)
+
+
+def main(url, period, timeout, regex):
+    results_queue = Queue()
+    stop = Event()
+    results_sender = Thread(target=kafka_producer, args=(results_queue, period))
+    results_sender.start()
+    scheduler = get_scheduler(period)
+
+    pollers_number = timeout // period or 1
+    pollers = [Poller(url, timeout, scheduler, stop, results_queue, regex) for i in
+               range(pollers_number)]
+
+    set_interrupt_handler(stop, pollers, results_queue, results_sender)
+
+    for poller in pollers:
+        poller.start()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('url')
     parser.add_argument('-p', '--period', type=int, help='Poll period in seconds', default=2)
+    parser.add_argument('-t', '--timeout', type=int, help='Timeout in seconds', default=10)
     parser.add_argument('-r', '--regex')
     args = parser.parse_args()
-
-    results_queue = Queue()
-    stop_event = Event()
-    results_sender = Thread(target=kafka_producer, args=(results_queue, args.period))
-    results_sender.start()
-
-    start = time.time()
-    while True:
-        try:
-            if time.time() >= start:
-                results_queue.put(json.dumps(check_website(args.url, args.regex)))
-                remainder = args.period - (time.time() - start)
-                start += args.period
-            else:
-                continue
-        except KeyboardInterrupt:
-            results_queue.put(None)
-            break
-        except requests.exceptions.ConnectionError:
-            results_queue.put(None)
-            raise
+    main(args.url, args.period, args.timeout, args.regex)
